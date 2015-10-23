5922b4cdb16ad5999a9fe07c1611548a8b1fa60d
tokikaze0604 <tokikaze0604@gmail.com>
2015-10-24 02:22:15 +0900

    model.js作成

---
+++ model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost/user';
+var db  = mongoose.createConnection(url, function(err, res){
+  if(err){
+    console.log('Error connected: ' + url + ' - ' + err);
+  }else{
+    console.log('Success connected: ' + url);
+  }
+});
+
+// Modelの定義
+var UserSchema = new mongoose.Schema({
+  email    : String,
+  password  : String
+},{collection: 'info'});
+
+exports.User = db.model('User', UserSchema);
